32ecd395dd76192f64e17b9e7b816b0c90d2d536
Mehdi Heidari <hymehdi84@gmail.com>
2025-02-16 20:14:24 +0100

    new() contraint learned

--- CSharpFeaturesTraining/Generics/Repositories/GenericRepository.cs
+++ CSharpFeaturesTraining/Generics/Repositories/GenericRepository.cs
@@ -2,10 +2,14 @@
 
 namespace CSharpFeaturesTraining.Repositories;
 
-public class GenericRepository<T> where T : class ,IEntityBase
+public class GenericRepository<T> where T : class ,IEntityBase,new()
 {
     protected readonly List<T> _items = new List<T>();
 
+    public T CreateItem()
+    {
+       return new T();
+    }
     public T? GetById(int id)
     {
         return _items.Single(c => c.Id == id);
@@ -23,7 +27,7 @@ public class GenericRepository<T> where T : class ,IEntityBase
     }
 }
 
-public class GenericRepositoryWithRemove<T> : GenericRepository<T> where T : EntityBase
+public class GenericRepositoryWithRemove<T> : GenericRepository<T> where T : EntityBase,new()
 {
     public void Remove(T item) => _items.Remove(item);
 }
